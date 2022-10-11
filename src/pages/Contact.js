ad800e87eeb03bfa972ae22ce59a6a38f94b475a
Genemichael25 <genemichael25@yahoo.com>
2022-10-11 16:17:20 -0700

    Updated contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { SiMinutemailer } from "react-icons/si";
 
 const Contact = () => {
   return (
     <>
-      <h1>Under Construction Contact</h1>
-      <p>I love traveling, learning about different cultures and listening to people's stories. Feel free to get in touch even if it's not software development related. We can chat about travel, sports, home improvements, food, business ideas, etc!!! </p>
+      <h1>Contact Me 😬</h1>
+      <div className="contact">
+      <p >I love traveling, learning about different cultures and listening to people's stories. Feel free to get in touch even if it's not software development related. We can chat about travel, sports, home improvements, food, business ideas, etc!!! </p>
+      <p>This tech advanced paper airplane delivers messages in secs!!</p>
+      <a href="mailto:genemartinez.dev@gmail.com">
+        <SiMinutemailer size="4rem" color="black"/>
+        genemartinez.dev@gmail.com
+      </a>
+      </div>
     </>
-  );
-};
+  )
+}
 
-export { Contact };
+export { Contact }
