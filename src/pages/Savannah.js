9603faafa3c7821df21830cbf144e8f08f0fce42
Genemichael25 <genemichael25@yahoo.com>
2022-10-14 15:08:37 -0700

    Made Changes to Savannah page and added redirect

--- src/pages/Savannah.js
+++ src/pages/Savannah.js
@@ -3,11 +3,27 @@ import React from 'react'
 const Savannah = () => {
   return (
     <>
-    <div>Savannah</div>
-      <h2>What is the app?</h2>
-      <h2>Tech used?</h2>
-      <h2>Challenges?</h2>
-      <h2>Outcome?</h2>
+    <h2>Savannah Tinder</h2>
+      <h4>What is it?</h4>
+      <div className='info'>
+        <p>Similar to another popular app, but for animals! This is two separate applications, a React frontend to handle the views and a Rails API that handles the data.
+        <br/>
+        </p>
+      </div>
+      <h4>Features</h4>
+      <div className='features'>
+        <p style={{marginRight:"50px"}}> One of the main features of our app was to allow users to view others. Our meet our members link takes you to a page where all users are displayed with there image, name and age. Someone catch your eye? Click About Me and find out more!</p>
+        <img src='/STindex.png' alt='ST index' width="45%" />
+      </div>
+      <div className='features'>
+        <img src='/STshow.png' alt='ST show' width="45%" />
+        <p>Once a user wants more information about a certain member, they are taken to a page where only that member is displayed. By using string interpolation, we can give a friendly intro about that member and what they enjoy doing. A members info can also be updated or deleted from this page.</p>
+      </div>
+      <h4>Challenges & Lessons Learned</h4>
+      <div className='info'>
+        <li>Styling prebuilt components doesn't always go as planned.</li>
+        <p><strong>Learned:</strong> Some components didn't respond to styling applied in App.css file. We were able to get the look we wanted by using some inline styling.</p>
+      </div>
     </>
   )
 }
